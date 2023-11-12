3683401f77df1f42f73810100f0477b5f8e9651d
Nguyen Van Manh <113473329+ManhNV203@users.noreply.github.com>
2023-11-12 21:03:16 +0700

    update info user login #1

--- back-end/src/main/java/com/example/backend/core/security/dto/UsersDTO.java
+++ back-end/src/main/java/com/example/backend/core/security/dto/UsersDTO.java
@@ -1,20 +1,25 @@
 package com.example.backend.core.security.dto;
 
-import com.example.backend.core.security.config.custom.CustomUserDetails;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.Date;
 
 
 @Getter @Setter
 @NoArgsConstructor @AllArgsConstructor
 public class UsersDTO {
     private Long id;
-    private String username;
+    private String code;
+    private String fullname;
+    private Date birthday;
+    private String gender;
+    private String phone;
     private String email;
-    private Integer id_customer;
-    private Integer id_staff ;
+    private String username;
     private String role;
-
+    private String isdn;
 }
