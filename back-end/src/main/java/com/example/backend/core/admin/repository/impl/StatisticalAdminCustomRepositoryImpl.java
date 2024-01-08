949c93e7389ca0d4542e6d902954ddbc55e1a009
quanghung.le <quanghung.le@migitek.com>
2024-01-08 21:22:00 +0700

    update BE - hunglqph20358

--- back-end/src/main/java/com/example/backend/core/admin/repository/impl/StatisticalAdminCustomRepositoryImpl.java
+++ back-end/src/main/java/com/example/backend/core/admin/repository/impl/StatisticalAdminCustomRepositoryImpl.java
@@ -37,10 +37,10 @@ public class StatisticalAdminCustomRepositoryImpl implements StatisticalAdminCus
             sql.append("  SELECT \n" +
                     "    DateRange.DateValue,\n" +
                     "    count(o.id) AS totalQuantity,\n" +
-                    "    coalesce(SUM(o.total_payment), 0.00) AS TotalPayment\n" +
+                    "    coalesce(SUM(o.total_payment), 0.00) AS TotalPayment,  COALESCE(SUM(od.quantity), 0) AS TotalQuantity  \n" +
                     "FROM DateRange  ");
             sql.append("  LEFT JOIN `order` o ON DATE(o.payment_date) = DateRange.DateValue and o.status = 3\n" +
-                    "GROUP BY DateRange.DateValue\n" +
+                    "LEFT JOIN order_detail od ON o.id = od.id_order  GROUP BY DateRange.DateValue\n" +
                     "ORDER BY DateRange.DateValue; ");
             Query query =entityManager.createNativeQuery(sql.toString());
             query.setParameter("dateFrom", statisticalAdminDTO.getDateFrom());
@@ -51,6 +51,7 @@ public class StatisticalAdminCustomRepositoryImpl implements StatisticalAdminCus
                 dto.setDateStr((String) o[0]);
                 dto.setQuantityOrder((Long) o[1]);
                 dto.setRevenue((BigDecimal) o[2]);
+                dto.setQuantityProduct(((BigDecimal) o[3]).longValue());
                 lst.add(dto);
             }
         }catch (Exception e){
